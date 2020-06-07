ea624fecddcb5aa08cbd87f33b92c9082de7be7b
EC2 Default User <ec2-user@ip-172-31-44-147.us-east-2.compute.internal>
2020-06-07 10:47:45 +0000

    Adding the Files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=udacitypinzonimage
+dockerpath=demolocal
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run $dockerpath --image=andresaaap/udacitypinzonimage:v1pro3 --port=80
+kubectl run $dockerpath --image=nouranelkassas/demolocal --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -17,5 +17,5 @@ kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment udacitypinzonimage --type=LoadBalancer --port=80
-minikube service udacitypinzonimage
+kubectl expose deployment demolocal --type=LoadBalancer --port=80
+minikube service demolocal
