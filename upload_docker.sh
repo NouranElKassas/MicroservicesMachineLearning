3106111f6d7a5782c978c7a9e5ab7fbb093a54b3
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-07 11:16:57 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -12,7 +12,7 @@ dockerpath=demolocal
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username demolocal
-docker tag udacitypinzonimage nouranelkassas/demolocal:v1pro3
+docker tag udacitypinzonimage nouranelkassas/demolocal:latest
 # Step 3:
 # Push image to a docker repository
-docker push nouranelkassas/demolocal:v1pro3
+docker push nouranelkassas/demolocal:latest
